5efa15b66c761f96a9d0d91b363ed41b4cff78ca
Shanjin Yang <shanjin.yang@aispeech.com>
2016-09-22 16:17:30 +0800

    implement music list destroy
    Signed-off-by: Shanjin Yang <shanjin.yang@aispeech.com>

--- test_case.c
+++ test_case.c
@@ -120,10 +120,11 @@ music_info *music_next_get(music_obj *obj)
 
 music_info *music_prev_get(music_obj *obj)
 {
-	music_info *tmp = list_entry(obj->cur_music->list.prev,
+	music_info *prev = list_entry(obj->cur_music->list.prev,
 					music_info,
 					list);
-	return (tmp)? tmp: NULL;
+	music_info *cur = music_cur_get(obj);
+	return (prev == cur)? NULL: prev;
 }
 
 int music_list_delete(music_info *info)
@@ -206,26 +207,72 @@ int music_info_init(music_info **info, char *title, char *artist, char *url)
 	return 0;
 }
 
+int music_list_destroy(music_obj *obj)
+{
+	LIST *tmp = &obj->head.list;
+	music_info *m;
+	while (!is_list_last(tmp)) {
+		m = list_entry(tmp, music_info, list);
+		tmp = tmp->next;
+		music_list_delete(m);
+	}
+	m = list_entry(tmp, music_info, list);
+	music_list_delete(m);
+
+	return 0;
+}
+
+/*
+==11124== 
+==11124== HEAP SUMMARY:
+==11124==     in use at exit: 8,000 bytes in 200 blocks
+==11124==   total heap usage: 800 allocs, 600 frees, 9,200 bytes allocated
+==11124== 
+==11124== LEAK SUMMARY:
+==11124==    definitely lost: 4,000 bytes in 100 blocks
+==11124==    indirectly lost: 3,960 bytes in 99 blocks
+==11124==      possibly lost: 0 bytes in 0 blocks
+==11124==    still reachable: 40 bytes in 1 blocks
+==11124==         suppressed: 0 bytes in 0 blocks
+==11124== Rerun with --leak-check=full to see details of leaked memory
+==11124== 
+==11124== For counts of detected and suppressed errors, rerun with: -v
+==11124== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+*/
+int test_music_next_get()
+{
+	int i = 100;
+	while (i--) {
+		music_list_init(&g_m, 20);
+		music_info *tmp;
+		music_info_init(&tmp, "a", "b", "c");
+		music_list_insert(&g_m, tmp);
+
+		music_info_init(&tmp, "d", "e", "f");
+		music_list_insert(&g_m, tmp);
+			
+		tmp = music_cur_get(&g_m);
+		printf("cur: %s\n", tmp->artist);
+		tmp = music_next_get(&g_m);
+		if (tmp == NULL) {
+			printf("no next music\n");
+			//goto end;
+		} else {
+			printf("cur: %s\n", tmp->artist);
+		}
+		music_list_destroy(&g_m);
+	}
+	return 0;
+}
+
+int test_music_prev_get()
+{
+	return 0;
+}
+
 int main()
 {
 	test_insert_delete_list(10, 20, 30, 30, 30);
-
-	music_list_init(&g_m, 20);
-	music_info *tmp;
-	music_info_init(&tmp, "a", "b", "c");
-	music_list_insert(&g_m, tmp);
-
-	music_info_init(&tmp, "d", "e", "f");
-	music_list_insert(&g_m, tmp);
-		
-	tmp = music_cur_get(&g_m);
-	printf("cur: %s\n", tmp->artist);
-	tmp = music_next_get(&g_m);
-	if (tmp == NULL) {
-		printf("no next music\n");
-		goto end;
-	}
-	printf("cur: %s\n", tmp->artist);
-end:
+	test_music_next_get();
 	return 0;
 }
